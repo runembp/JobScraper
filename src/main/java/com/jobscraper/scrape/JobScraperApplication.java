0cab16bbb0e85998e6e742fba4151c1ea7c071c8
runembp <58623004+runembp@users.noreply.github.com>
2021-04-30 12:13:45 +0200

    Commit Test

--- src/main/java/com/jobscraper/scrape/JobScraperApplication.java
+++ src/main/java/com/jobscraper/scrape/JobScraperApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobScraperApplication
 {
-    //COMMIT TEST TO WUHU
+    //JENKINS TEST
     public static void main(String[] args)
     {
         SpringApplication.run(JobScraperApplication.class, args);
