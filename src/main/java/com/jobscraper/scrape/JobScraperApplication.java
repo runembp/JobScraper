eded12e6346de2c99341b9593f7099ba04bb0cf1
runembp <58623004+runembp@users.noreply.github.com>
2021-04-30 09:24:49 +0200

    Tilføjet SQL Server

--- src/main/java/com/jobscraper/scrape/JobScraperApplication.java
+++ src/main/java/com/jobscraper/scrape/JobScraperApplication.java
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobScraperApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(JobScraperApplication.class, args);
     }
-
 }
