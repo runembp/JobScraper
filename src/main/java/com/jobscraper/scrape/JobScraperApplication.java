9b8358ddef5db5175a3052fb0636f1a3b6a42410
runembp <58623004+runembp@users.noreply.github.com>
2021-05-18 11:58:41 +0200

    Hurra for Jon

--- src/main/java/com/jobscraper/scrape/JobScraperApplication.java
+++ src/main/java/com/jobscraper/scrape/JobScraperApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobScraperApplication
 {
-    // HURRA FOR JON - HUSK 12 TALLERNE
+    // HURRA FOR JON - HUSK 12 TALLERNE - HURRA
     public static void main(String[] args)
     {
         SpringApplication.run(JobScraperApplication.class, args);
