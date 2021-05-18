db485e7d33d7341d42e811fa8939473d01595f9b
runembp <58623004+runembp@users.noreply.github.com>
2021-05-18 11:39:33 +0200

    Test Commit

--- src/main/java/com/jobscraper/scrape/JobScraperApplication.java
+++ src/main/java/com/jobscraper/scrape/JobScraperApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobScraperApplication
 {
+    // HELLO WORLD
     public static void main(String[] args)
     {
         SpringApplication.run(JobScraperApplication.class, args);
