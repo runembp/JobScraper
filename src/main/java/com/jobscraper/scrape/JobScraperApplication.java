7708da8f8275c593c3449134ac02745c3d1ffdd0
runembp <58623004+runembp@users.noreply.github.com>
2021-05-18 11:47:06 +0200

    Ændret vigtig tekst i Main Metoden

--- src/main/java/com/jobscraper/scrape/JobScraperApplication.java
+++ src/main/java/com/jobscraper/scrape/JobScraperApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobScraperApplication
 {
-    // HELLO WORLD
+    // HURRA FOR JON - HUSK 12 TALLERNE
     public static void main(String[] args)
     {
         SpringApplication.run(JobScraperApplication.class, args);
